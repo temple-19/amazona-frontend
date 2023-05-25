3a3223c2a7b95bb3f24a150ab5733721b1275b8a
Temple <tozoaz@gmail.com>
2023-05-25 17:43:02 +0100

    Update ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -37,7 +37,7 @@ export default function ProfileScreen() {
     e.preventDefault();
     try {
       const { data } = await axios.put(
-        '/api/users/profile',
+        'https://amazona-api-hfe1.onrender.com/api/users/profile',
         {
           name,
           email,
