fc27221108268b61bc7ad16451ae59d5d9d694cb
Temple <tozoaz@gmail.com>
2023-05-25 17:19:29 +0100

    Update CartScreen.js

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -19,7 +19,7 @@ export default function CartScreen() {
   } = state;
 
   const updateCartHandler = async (item, quantity) => {
-    const { data } = await axios.get(`/api/products/${item._id}`);
+    const { data } = await axios.get(`https://amazona-api-hfe1.onrender.com/api/products/${item._id}`);
     if (data.countInStock < quantity) {
       window.alert('Sorry. Product is out of stock');
       return;
