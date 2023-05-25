fbde042cd026796e5d6cbb71669c7c1efbc836b6
Temple <tozoaz@gmail.com>
2023-05-25 13:50:49 +0100

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,7 +17,7 @@ function Product(props) {
   const addToCartHandler = async (item) => {
     const existItem = cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${item._id}`);
+    const { data } = await axios.get(`https://amazona-api-hfe1.onrender.com/api/products/${item._id}`);
     if (data.countInStock < quantity) {
       window.alert('Sorry. Product is out of stock');
       return;
